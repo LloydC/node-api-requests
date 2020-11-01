576e38d960a5fc4556235ccbd4053ca7b3d12db8
Lloyd Chambrier <lloydchambrier@gmail.com>
2020-11-01 15:19:40 +0100

    Index view, controller and route created

---
+++ src/app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const hbs = require("hbs");
+const path = require("path");
+
+const homeRouter = require("../routes/home.routes");
+
+const app = express();
+
+// Static files
+app.use(express.static("public"));
+
+hbs.registerPartials(__dirname + "/views/partials");
+
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "../views"));
+
+// Setting to use routes
+app.use("/", homeRouter);
+
+module.exports = app;
