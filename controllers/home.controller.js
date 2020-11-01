d31efc0fd1fadeacf52031c94bdbdd2145444332
Lloyd Chambrier <lloydchambrier@gmail.com>
2020-11-01 21:35:22 +0100

    Matched country abbreviation and name

--- controllers/home.controller.js
+++ controllers/home.controller.js
@@ -1,7 +1,25 @@
-const cities = require('../data/cities.json');
-cities.sort()
+const cities = require('cities.json');
+const countries = require('../data/country-by-abbreviation.json');
 
 exports.renderIndex = (req, res) => {
-    res.render("index", {cities});
+  // sort cities alphabetically
+  const sortedCities = cities.sort(function(a, b){
+    if(a.name < b.name) { return -1; }
+    if(a.name > b.name) { return 1; }
+    return 0;
+})
+console.log(sortedCities[0])
+//map country name to cities
+ const citiesAndCountries = sortedCities.map(city => {
+   const country = countries.find( country => city.country === country.abbreviation)
+   return {
+     name: city.name,
+     country,
+     lng: city.lng,
+     lat: city.lat
+   }
+ });
+console.log(citiesAndCountries[0])
+    res.render("index", {cities: citiesAndCountries});
   };
   
